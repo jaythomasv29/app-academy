b196f1d15aa8bbd73c91ed81bc8d7e0b0061c5f0
James Vongampai <jaythomasv29@gmail.com>
2020-11-23 10:06:17 -0800

    practice using Ruby Rspec gem to create methods using test file

--- 01-swe-engineering-foundations-week1/01-02-01-rspec/rspec_exercise_1/lib/part_2.rb
+++ 01-swe-engineering-foundations-week1/01-02-01-rspec/rspec_exercise_1/lib/part_2.rb
@@ -1,4 +1,36 @@
 def hipsterfy(str)
-  vowels = "aeiou"
-  str.r
+  vowels = "aeiouAEIOU"
+  if str.split("").any? { |char| vowels.include?(char) }
+    str.slice!(str.rindex(/[aeiou]/))
+    return str
+  else
+    str
+  end
 end
+
+p hipsterfy("tonic")
+# p hipsterfy("panther")
+# p hipsterfy("swimming")
+# p hipsterfy("rhythm")
+# p hipsterfy("my")
+
+def vowel_counts(str)
+  result = Hash.new(0)
+  vowels = "aeiouAEIOU"
+  str.each_char { |char| result[char.downcase] += 1 if vowels.include?(char) }
+  result
+end
+
+def caesar_cipher(msg, n)
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  shifted = msg.split("").map do |char_ele|
+    if alphabet.include?(char_ele)
+      alphabet[(alphabet.index(char_ele) + n) % 26] # the index + n
+    else
+      char_ele
+    end
+  end
+  shifted.join("")
+end
+
+# p caesar_cipher("apple", 1)
